57ecd321c8455f0a72eeee33de72db0067945f7c
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-01-10 09:43:05 -0600

    completed variables and helloworld exercises

---
+++ src/FirstSteps/HelloWorld.java
@@ -0,0 +1,8 @@
+package FirstSteps;
+
+public class HelloWorld {
+    // Udemy 14, Challenge 1
+    public static void main(String[] args) {
+        System.out.println("Hello Tim");
+    }
+}
---
+++ src/FirstSteps/Variables.java
@@ -0,0 +1,8 @@
+package FirstSteps;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+}
