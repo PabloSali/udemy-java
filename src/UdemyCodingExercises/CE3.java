f99aaa5a0fbf3b7bca531fb6d90311a5bbabdcc8
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-01-17 17:53:20 -0600

    completed exercise 3 and started 4

--- src/UdemyCodingExercises/CE3.java
+++ src/UdemyCodingExercises/CE3.java
@@ -1,4 +1,16 @@
 package UdemyCodingExercises;
 
 public class CE3 {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+    }
+    public static void printMegaBytesAndKiloBytes(int kilobyte){
+        if(kilobyte < 0){
+            System.out.println("Invalid Value");
+        }else {
+            int megabyte = kilobyte / 1024;
+            int kbRemaining = kilobyte - (megabyte * 1024);
+            System.out.println(kilobyte + " KB = " + megabyte + " MB and " + kbRemaining + " KB");
+        }
+    }
 }
