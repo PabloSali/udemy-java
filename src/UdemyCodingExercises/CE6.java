2dd86eb5bcdf44c8bc349bcb65dd7f3f2183f5f1
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-01-17 19:03:55 -0600

    completed CE6

--- src/UdemyCodingExercises/CE6.java
+++ src/UdemyCodingExercises/CE6.java
@@ -1,4 +1,19 @@
 package UdemyCodingExercises;
 
 public class CE6 {
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
+        if((num1 >= 0 && num2 >= 0) || (num1 <= 0 && num2 <= 0)){
+            if (num1 <= 0 && num2 <= 0) {
+                double number1 = Math.ceil(num1 * 1000);
+                double number2 = Math.ceil(num2 * 1000);
+                return (number1 == number2);
+            }else {
+                double number1 = Math.floor(num1 * 1000);
+                double number2 = Math.floor(num2 * 1000);
+                return (number1 == number2);
+            }
+        }else{
+            return false;
+        }
+    }
 }
