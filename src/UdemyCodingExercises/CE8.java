3c9d2e1c756bba2c3a1c743042491a91a893498a
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2023-01-17 19:28:49 -0600

    completed CE 8

---
+++ src/UdemyCodingExercises/CE8.java
@@ -0,0 +1,7 @@
+package UdemyCodingExercises;
+
+public class CE8 {
+    public static boolean hasTeen(int num1, int num2, int num3){
+        return (num1 <= 19 && num1 >= 13) || (num2 <= 19 && num2 >= 13) || (num3 <= 19 && num3 >= 13);
+    }
+}
